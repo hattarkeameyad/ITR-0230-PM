4862bb72fd6377d8e944457468b4f9627d36005d
Mr Ameya Hattarke <ameyahattarke@gmail.com>
2026-06-13 11:26:52 +0530

    javascript

---
+++ JS/day1/script.js
@@ -0,0 +1,27 @@
+
+const pi=3.14;
+let b=20;
+let c=22.5;
+let name="asdf"+'a;lsdjflasdf';
+var d=25.20;
+var str="Ameya";
+console.log("This si value of b, this is value of pi "+ pi+" " +b );
+console.log(typeof b);
+console.log(typeof str);
+console.log(typeof c);
+console.log(c+b);
+console.log(c-b)
+console.log(c/b)
+console.log(c*b)
+let ans=255%10;
+console.log(ans);
+let p=20,q=15;
+if(p>q){
+    console.log("P is greater . . . .");
+}
+else{
+    console.log("Q is greater   b. . .");
+}
+
+
+
